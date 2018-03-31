da3157fad4ce680890b80f59f540a220346a551f
Michael Camara <michaeljcamara@gmail.com>
2018-03-31 15:23:45 -0400

    Created AllDependencies.cs with every type from unity sample project manually entered as fields (21 total types).

---
+++ Prototype/Assets/Scripts/AllDependencies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllDependencies  {
+    BackgroundParallax a;
+    BackgroundPropSpawner b;
+    Bomb c;
+    BombPickup d;
+    CameraFollow e;
+    Destroyer f;
+    Enemy g;
+    FollowPlayer h;
+    Gun i;
+    HealthPickup j;
+    LayBombs k;
+    Pauser l;
+    PickupSpawner m;
+    PlayerControl n;
+    PlayerHealth o;
+    Remover p;
+    Rocket q;
+    Score r;
+    ScoreShadow s;
+    SetParticleSortingLayer t;
+    Spawner u;
+}
